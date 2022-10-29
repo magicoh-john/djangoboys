799408c3387770c4a96b24e76d4b3a298b5f6353
magicoh-john <magicoh7@gmail.com>
2022-10-29 16:55:07 +0900

    Add view and template for detailed blog post as well as css for the site

--- blog/urls.py
+++ blog/urls.py
@@ -7,6 +7,7 @@ from . import views
 urlpatterns = [
     path('', views.post_index, name='post_index'),
     path('list', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail')
 ]
 
 '''
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # from 다음에 있는 마침표(.)는 현재 디렉토리 또는 애플리케이션
 from .models import Post
 from django.utils import timezone
+
 
 # Create your views here.
 
@@ -25,3 +27,9 @@ def post_list(request):
 render 메서드를 호출하여 받은(return) blog/post_list.html 템플릿을 보여줍니다.
 
 '''
+
+
+# 게시물 상세 페이지 처리 핸들러(뷰)
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
